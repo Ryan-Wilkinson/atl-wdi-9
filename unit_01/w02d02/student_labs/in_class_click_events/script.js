1604f611b5150d25ed8e4d05c6c6c1abc50d9334
Charles Ryan Wilkinson <snowlerpowered@gmail.com>
2017-03-02 11:18:59 -0500

    my js

--- unit_01/w02d02/student_labs/in_class_click_events/script.js
+++ unit_01/w02d02/student_labs/in_class_click_events/script.js
@@ -1,3 +1,31 @@
 window.onload = function() {
-  
+
+
+var firstButton = document.getElementById('firstButton');
+var secondButton = document.getElementById('secondButton');
+var thirdButton = document.getElementById('thirdButton');
+
+
+firstButton.addEventListener('click', function() {
+	alert('You have clicked button one!');
+	});
+
+
+secondButton.addEventListener('click', function() {
+	var paragraph = document.createElement("P");                      
+	var info = document.createTextNode("A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback).");
+	paragraph.appendChild(info);                                         
+	document.getElementById("Information").appendChild(paragraph);
+
+});
+
+thirdButton.addEventListener('click', function() {
+	var divsBaby = document.getElementById("Information");
+	while (divsBaby.hasChildNodes()) {
+		divsBaby.removeChild(divsBaby.firstChild);
+	}
+
+});
+
+
 }
