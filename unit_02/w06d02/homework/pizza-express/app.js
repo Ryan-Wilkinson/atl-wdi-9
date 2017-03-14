4ca5e4df6f732c5534f8fbf9cbba8747a8641f52
Ryan Wilkinson <ryanw@Ryans-MacBook-Pro.local>
2017-03-14 18:59:02 -0400

    finished HW

--- unit_02/w06d02/homework/pizza-express/app.js
+++ unit_02/w06d02/homework/pizza-express/app.js
@@ -5,20 +5,28 @@ var port = 3000;
 
 app.set("view engine", "hbs");
 app.set('views', './views');
+app.use(express.static(__dirname + '/public')); // VERY IMPORTANT!! Make sure to add a '/'
 
 app.get('/', function(req, res) {
-  res.send('<h1>Welcome To Pizza Express!!</h1>');
+	res.render
+})
+
+app.get('/topping/:type', function(req, res) {
+  res.render('topping', {
+    topping: req.params.type
+  })
 });
 
-app.get('/topping/:type', function(req, res, next) {
-    res.send(`${req.params.type} Pizza, Good choice!`);
+app.get('/order/:amount/:size', function(req, res) {
+  res.render('order', {
+    amount: req.params.amount,
+    size: req.params.size
+  })
 });
 
-app.get('/order/:amount/:size', function(req, res, next) {
-   res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in one minute!`);
+app.get('/pizza', function(req, res) {
+  res.status(404).send('<h1>ERROR 404 Not Found</h1>');
 });
-
-
 
 app.listen(port, function(){
   console.log("==========================")
